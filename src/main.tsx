2e0dc9d5dee814e5a747ae45fa8b238883bc324f
elma2134 <gun8e88@gmail.com>
2026-05-08 20:38:03 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./styles/index.css";
 /* Facebook SDK */
 const fbScript = document.createElement("script");
 fbScript.src =
-  "https://www.facebook.com/Happycatspagroomingdogcare/";
+  "https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v19.0";
 fbScript.async = true;
 fbScript.defer = true;
 fbScript.crossOrigin = "anonymous";
